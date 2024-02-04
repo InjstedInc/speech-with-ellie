cb2455fbaa233ed6e1a9156ed88af7e12340069a
cristhiansoria <crisso2292@gmail.com>
2024-02-04 12:51:37 -0500

    Made Speech with Ellie

---
+++ components/ComingSoon/index.tsx
@@ -0,0 +1,10 @@
+export default function ComingSoon() {
+    return (
+        <div className="flex items-center justify-center w-screen h-screen bg-gray-800 text-white">
+            <div>
+                <h1 className="text-4xl font-bold">Speech with Ellie</h1>
+                <p className="mt-4 text-xl">Coming Soon!</p>
+            </div>
+        </div>
+    );
+}
