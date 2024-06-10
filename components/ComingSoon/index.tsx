338f1c3d491de2960dd10aefa191793438494083
Cristhian Soria <crisso2292@gmail.com>
2024-06-10 19:03:36 -0400

    refactor: expanding eslint configs, enabling prettier, and adding husky

--- components/ComingSoon/index.tsx
+++ components/ComingSoon/index.tsx
@@ -1,13 +1,12 @@
 export default function ComingSoon() {
-    const url = '/images/coming-soon.jpg'
-    return (
-        <div
-            className="flex items-center justify-center w-screen h-screen bg-cover bg-center text-white"
-            style={{ backgroundImage: `url('/images/ellies.png')` }}
-        >
-            <div className="bg-gray-900 bg-opacity-75 p-10 rounded-lg">
-                <h1 className="text-4xl font-bold">Coming Soon!</h1>
-            </div>
-        </div>
-    );
+  return (
+    <div
+      className='flex items-center justify-center w-screen h-screen bg-cover bg-center text-white'
+      style={{ backgroundImage: `url('/images/ellies.png')` }}
+    >
+      <div className='bg-gray-900 bg-opacity-75 p-10 rounded-lg'>
+        <h1 className='text-4xl font-bold'>Coming Soon!</h1>
+      </div>
+    </div>
+  )
 }
