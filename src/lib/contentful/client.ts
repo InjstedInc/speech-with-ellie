7b46243b9fc7142250461578f12dc03c675af20f
Cristhian Soria <crisso2292@gmail.com>
2024-06-14 02:07:44 -0400

    refactor!: changing the name of env vars to fix errors in Vercel

--- src/lib/contentful/client.ts
+++ src/lib/contentful/client.ts
@@ -2,11 +2,11 @@ import { createClient, CreateClientParams } from 'contentful'
 import ContentfulConfigError from '@/src/lib/contentful/error/ContentfulConfigError'
 
 function getContentfulConfig(): CreateClientParams {
-  const space = process.env.CONTENTFUL_SPACE
+  const space = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID
   const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN
   if (!space || !accessToken) {
     throw new ContentfulConfigError(
-      `The \`${!space ? 'CONTENTFUL_SPACE' : 'CONTENTFUL_ACCESS_TOKEN'}\` environment variable is required to connect to Contentful`,
+      `The \`${!space ? 'NEXT_PUBLIC_CONTENTFUL_SPACE_ID' : 'CONTENTFUL_ACCESS_TOKEN'}\` environment variable is required to connect to Contentful`,
     )
   }
   return { space, accessToken }
